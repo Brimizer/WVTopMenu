dba17727927b77d0294ab459ca3904f921c6a6dd
Daniel Brim <brimizer@gmail.com>
2014-04-22 23:38:43 -0700

    Adding header file

---
+++ WVTopMenu/WVTopMenu.h
@@ -0,0 +1,15 @@
+//
+//  WVTopMenu.h
+//  TestBench
+//
+//  Created by Daniel Brim on 4/22/14.
+//  Copyright (c) 2014 Daniel Brim. All rights reserved.
+//
+
+#ifndef _WVTopMenu_h
+#define _WVTopMenu_h
+
+#import "WVTopMenuContainerViewController.h"
+#import "UIViewController+WVTopMenuAdditions.h"
+
+#endif
